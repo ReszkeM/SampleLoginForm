c95853961b5465e0f2007432f44c526eadab37e6
Marek Reszke <MReszke@newforma.com>
2019-08-03 12:20:08 +0200

    fixes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './home/containers/home';
 import Login from './login/containers/login';
@@ -11,15 +11,6 @@ class App extends React.Component<{}, {}> {
     return (
       <Router>
         <div className="container">
-          <ul className="header">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-
           <div className="body">
             <Route exact={true} path="/" component={Home} />
             <Route path="/login" component={Login} />
