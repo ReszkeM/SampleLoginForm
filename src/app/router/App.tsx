5a1e6ec674add386a78d985f1e321a0d534c5668
Marek Reszke <MReszke@newforma.com>
2019-08-03 12:25:54 +0200

    update structure

--- src/app/router/App.tsx
+++ src/app/router/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import Home from './home/containers/home';
-import Login from './login/containers/login';
+import Home from '../home/containers/home';
+import Login from '../login/containers/login';
 
 import './App.sass';
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './app/App';
+import App from './app/router/App';
 import * as serviceWorker from './app/shared/utils/serviceWorker';
 
 import './assets/styles/index.sass';
